e63c1f3abd4c16c2563d18abbcb412034a31889a
Nick Maludy <nmaludy@gmail.com>
2021-05-19 11:09:02 -0400

    Adding basic client installation

--- manifests/client/config.pp
+++ manifests/client/config.pp
@@ -2,6 +2,7 @@ class restic::client::config (
   String $dir = '/etc/restic',
   String $config_path = "${dir}/restic.conf",
   String $password_file_path = "${dir}/restic.pass",
+  String $template = 'restic/etc/restic.conf.epp',
   String $mode = '0600',
   String $owner = 'root',
   String $group = 'root',
@@ -14,18 +15,26 @@ class restic::client::config (
     group  => $group,
     mode   => '0750',
   }
+
   file { $config_path:
     ensure => directory,
     owner  => $owner,
     group  => $group,
     mode   => $mode,
+    content => epp($template, {
+      variables => {
+        RESTIC_REPOSITORY => $default_repo,
+        RESTIC_PASSWORD_FILE => $password_file_path,
+      },
+    }),
   }
+
   file { $password_file_path:
     ensure    => directory,
     owner     => $owner,
     group     => $group,
     mode      => $mode,
     show_diff => false,
-    contents  => $password,
+    content   => "$password\n",
   }
 }
