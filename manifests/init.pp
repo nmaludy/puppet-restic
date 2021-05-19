0604782aefc7d17b95d7808d058b22c33dff500a
Nick Maludy <nmaludy@gmail.com>
2021-05-19 11:20:05 -0400

    Add repo management flag

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,11 @@ class restic (
   String $client_package_name = 'restic',
   String $client_package_ensure = 'present',
   Boolean $server = false,
+  Boolean $repo_manage = true,
 ) {
+  if $repo_manage {
+    contain restic::repo
+  }
   if $client {
     contain restic::client
   }
