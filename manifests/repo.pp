b1ca89589ffb6d45c5dd22606fb000c34e99ce1e
Nick Maludy <nmaludy@gmail.com>
2021-05-19 13:00:26 -0400

    Try using yumrepo

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -2,20 +2,13 @@ class restic::repo (
   Boolean $manage = $restic::repo_manage,
 ) inherits restic {
   if $manage and $facts['os']['family'] == 'RedHat' {
-    $repo_path = '/etc/yum.repos.d/copart-restic-epel.repo'
-    $repo_section = 'copr:copr.fedorainfracloud.org:copart:restic'
+    $repo_name = 'copart-restic-epel'
+    $repo_path = "/etc/yum.repos.d/${repo_name}.repo"
     $os_maj = $facts['os']['release']['major']
-    # file { $repo_path:
-    #   ensure  => file,
-    #   owner   => 'root',
-    #   group   => 'root',
-    #   mode    => '0644',
-    # }
 
-    yumrepo { 'copart-restic-epel':
+    yumrepo { $repo_name:
       descr               => 'Copr repo for restic owned by copart',
       baseurl             => "https://download.copr.fedorainfracloud.org/results/copart/restic/epel-${os_maj}-\$basearch/",
-      type                => 'rpm-md',
       skip_if_unavailable => 'True',
       gpgcheck            => '1',
       gpgkey              => 'https://download.copr.fedorainfracloud.org/results/copart/restic/pubkey.gpg',
@@ -23,17 +16,18 @@ class restic::repo (
       enabled             => '1',
       enabled_metadata    => '1',
     }
-    # $settings_hash = {
-    #   'name' => ,
-    # }
-    # $settings_hash.each |$setting, $value| {
-    #   ini_setting { "${repo_path}:${repo_section}:${setting}":
-    #     ensure  => present,
-    #     path    => $repo_path,
-    #     section => $repo_section,
-    #     setting => $setting,
-    #     value   => $value,
-    #   }
-    # }
+    $settings_hash = {
+      'type' => 'rpm-md',
+    }
+    $settings_hash.each |$setting, $value| {
+      ini_setting { "${repo_path}:${repo_name}:${setting}":
+        ensure  => present,
+        path    => $repo_path,
+        section => $repo_name,
+        setting => $setting,
+        value   => $value,
+        require => Yumrepo[$repo_name],
+      }
+    }
   }
 }
